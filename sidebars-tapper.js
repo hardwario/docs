d6bc58767a244d757af8efa7c5d3a72a24be3ab0
Martin <81148675+Martian-0007@users.noreply.github.com>
2025-05-26 10:56:07 +0200

    Add MQTT specification for TAPPER
    Signed-off-by: Martin <81148675+Martian-0007@users.noreply.github.com>

--- sidebars-tapper.js
+++ sidebars-tapper.js
@@ -2,7 +2,7 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  docs: ["introduction", "installation", "usage"],
+  docs: ["introduction", "installation", "usage", "api-spec"],
 };
 
 module.exports = sidebars;
