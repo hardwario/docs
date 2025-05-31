82ae335c30ea038fb67cbbfa1e77e4af96f63c98
Martian <81148675+Martian-0007@users.noreply.github.com>
2025-05-31 22:58:39 +0200

    Reodred TAPPER Sidebar
    Signed-off-by: Martian <81148675+Martian-0007@users.noreply.github.com>

--- sidebars-tapper.js
+++ sidebars-tapper.js
@@ -2,7 +2,7 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  docs: ["introduction", "installation", "hardware", "usage", "tls-setup", "api-spec"],
+  docs: ["introduction", "hardware", "installation", "usage", "tls-setup", "api-spec"],
 };
 
 module.exports = sidebars;
