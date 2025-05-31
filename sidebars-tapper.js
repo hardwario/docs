c3bbe86e6602fa6d9e6e043f7be775f2f6b14cea
Martian <81148675+Martian-0007@users.noreply.github.com>
2025-05-31 22:45:32 +0200

    Reorder TAPPER sidebar
    Signed-off-by: Martian <81148675+Martian-0007@users.noreply.github.com>

--- sidebars-tapper.js
+++ sidebars-tapper.js
@@ -2,7 +2,7 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  docs: ["introduction", "installation", "hardware", "tls-setup" ,"usage", "api-spec"],
+  docs: ["introduction", "installation", "hardware", "usage", "tls-setup", "api-spec"],
 };
 
 module.exports = sidebars;
